b94f511eabf0f5237523e4a2992b46b7d0d0d911
luc4sleite <lucahleite@gmail.com>
2023-04-13 11:05:00 -0300

    fix: estilização do form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,5 @@
+import Input from "../Input/Input"
+
 const Form = ({ onSubmit }) => {
     const safeSubmit = event => {
         event.preventDefault()
@@ -6,10 +8,10 @@ const Form = ({ onSubmit }) => {
         const email = event.target[1].value
         onSubmit({ name, email})
     }
-    return <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center gap-10 -mt-20">
-        <input type="text" placeholder="Insira seu nome..." />
-        <input type="email" placeholder="Insira seu e-mail..." />
-        <button type="submit" className="bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 py-1 px-5 uppercase">Seguir</button>
+    return <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5">
+        <Input type="text" required placeholder="Insira seu nome..." />
+        <Input type="email" required placeholder="Insira seu e-mail..." />
+        <button type="submit" className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200  uppercase outline-none">Seguir</button>
     </form>
 }
 
