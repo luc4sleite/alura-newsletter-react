fe16e05cef2fe29519cd4e539cf3a221ec3da310
luc4sleite <lucahleite@gmail.com>
2023-04-13 12:07:10 -0300

    otimização para o deploy

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -8,7 +8,7 @@ const Article = ({ title, text, tags, image, alt}) => {
         </div>
         <div className="grid gap-1">
             {
-                text.map((content,index) => <p key={index} className="text-alura-200 dark:text-gray-400">{content}</p>)
+                text.map((content,index) => <p key={index} className="text-alura-200 dark:text-gray-400 line-clamp-6 sm:line-clamp-none">{content}</p>)
             }
         </div>
         { image && <img className="p-4" src={image} />}
